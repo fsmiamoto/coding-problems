a09d2a423b65d6467036cac3e4d129dcb163191d
Francisco Miamoto <francisco@dorayaki.co>
2022-04-02 20:28:43 -0300

    update product except self

--- leetcode/238.py
+++ leetcode/238.py
@@ -1,23 +1,21 @@
 from typing import List
 
 # For each position the result would be:
-# result[i] = prefix[i] * postfix[i]
+# result[i] = left[i] * right[i]
 # Instead of creating two new arrays, lets use the space
 # we've allocated for the result array already.
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-      n = len(nums)
-      result = [1] * n
+      left = 1
+      result = []
+      for i in range(len(nums)):
+        result.append(left)
+        left *= nums[i]
 
-      prefix = 1
-      for i in range(n):
-        result[i] = prefix
-        prefix *= nums[i]
-
-      postfix = 1
-      for i in range(n-1, -1, -1):
-        result[i] *= postfix
-        postfix *= nums[i]
+      right = 1
+      for i in range(len(nums)-1,-1,-1):
+        result[i] *= right
+        right *= nums[i]
 
       return result

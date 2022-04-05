f2611db556455c55fe305dad619fea1b112785d5
Francisco Miamoto <francisco@dorayaki.co>
2022-04-05 08:39:42 -0300

    improve connect level siblings

--- leetcode/116.py
+++ leetcode/116.py
@@ -1,38 +1,31 @@
 from collections import deque
+from typing import Optional
 
-"""
 # Definition for a Node.
 class Node:
-    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+    def __init__(self, val: int = 0, left: 'Optional[Node]' = None, right: 'Optional[Node]' = None, next: 'Optional[Node]' = None):
         self.val = val
         self.left = left
         self.right = right
         self.next = next
-"""
-
-# Not using constant space tough
-# But since this is a iterative solution, we don't have the extra call stack memory so I'd say it's fine :upside_down_face:;x
 
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        queue = deque()
+        if not root:
+            return root
 
-        if root:
-            queue.append(root)
-        previousNode = None
+        queue = deque()
+        queue.append(root)
 
         while queue:
-            n = len(queue)
-            for _ in range(n):
+            levelCount = len(queue)
+            prevNode = None
+            for _ in range(levelCount):
                 node = queue.popleft()
-                if previousNode:
-                    previousNode.next = node
-                previousNode = node
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-            previousNode = None
+                if prevNode:
+                    prevNode.next = node
+                if node.left: queue.append(node.left)
+                if node.right: queue.append(node.right)
+                prevNode = node
 
         return root
-

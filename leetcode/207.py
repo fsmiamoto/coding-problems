01359629d053ad4e647735004af057134cc0613c
Francisco Miamoto <francisco@dorayaki.co>
2022-04-23 15:37:11 -0300

    update course schedule

--- leetcode/207.py
+++ leetcode/207.py
@@ -1,8 +1,44 @@
-from collections import deque
+from collections import deque, defaultdict
 from typing import List
 
 class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+    # Cycle detection
+    def canFinish(self, numCourses:int, prerequisites: List[List[int]]) -> bool:
+      # Build a Graph
+      # Traverse the graph using DFS
+      # If a cycle is detected, return False
+
+      graph = defaultdict(list)
+
+      for [before,after] in prerequisites:
+        graph[after].append(before)
+
+      visited, stack = set(), set()
+
+      def hasCycle(node, graph, visited, stack) -> bool:
+        visited.add(node)
+        stack.add(node)
+
+        for neighbor in graph[node]:
+          if neighbor not in visited:
+            if hasCycle(neighbor, graph, visited, stack):
+              return True
+          elif neighbor in stack:
+            return True
+
+        stack.remove(node)
+
+        return False
+
+      for course in range(numCourses):
+        if course not in visited:
+          if hasCycle(course, graph, visited, stack):
+            return False
+
+      return True
+
+    # Topo sort, kinda wasteful though
+    def _canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
       if len(prerequisites) < 2:
         return True
 
